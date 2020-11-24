6b87ace6b8b46a50eaf61595b00659248f841011
Harvey Wu <Harvey.Wu@quantatw.com>
2020-11-24 19:43:43 +0800

    modified code to fit coding standard & fix function parameter

--- json/buildjson.hpp
+++ json/buildjson.hpp
@@ -5,34 +5,34 @@
 #include <map>
 
 /**
- * Parse sensor config from json data.
- *
- * @param[in] data - json data.
- * @return Json data.
- */
-std::map<std::string, struct conf::sensorConfig>
+  * Parse sensor config from json data.
+  *
+  * @param[in] data - json data.
+  * @return Json data.
+  */
+std::map<std::string, struct conf::SensorConfig>
     getSensorInfo(const nlohmann::json& data);
 
 /**
- * Parse sku config from json data.
- *
- * @param[in] data - json data.
- * @return Sku config.
- */
-std::map<int, conf::skuConfig>
+  * Parse sku config from json data.
+  *
+  * @param[in] data - json data.
+  * @return Sku config.
+  */
+std::map<int, conf::SkuConfig>
     getSkuInfo(const nlohmann::json& data);
 
 /**
- * Validate json data.
- *
- * @param[in] data - json data.
- */
+  * Validate json data.
+  *
+  * @param[in] data - json data.
+  */
 void validateJson(const nlohmann::json& data);
 
 /**
- * Parse json file.
- *
- * @param[in] path - json file path.
- * @return Json data.
- */
+  * Parse json file.
+  *
+  * @param[in] path - json file path.
+  * @return Json data.
+  */
 nlohmann::json parseValidateJson(const std::string& path);
--- util/util.hpp
+++ util/util.hpp
@@ -7,62 +7,62 @@
 #include "conf.hpp"
 
 /**
- * Determine sku type.
- *
- * @return Sku type of the system.
- */
+  * Determine sku type.
+  *
+  * @return Sku type of the system.
+  */
 int getSkuNum();
 
 /**
- * Get sensor temp from dbus.
- *
- * @param[in] sensorDbusPath - sensor dbus path.
- * @return Sensor temp.
- */
+  * Get sensor temp from dbus.
+  *
+  * @param[in] sensorDbusPath - sensor dbus path.
+  * @return Sensor temp.
+  */
 double getSensorDbusTemp(std::string sensorDbusPath);
 
 /**
- * Get spec temp.
- *
- * @param[in] config - sensor config.
- * @return Spec temp of the sensor.
- */
-double getSpecTemp(struct conf::sensorConfig config);
+  * Get spec temp.
+  *
+  * @param[in] config - sensor config.
+  * @return Spec temp of the sensor.
+  */
+double getSpecTemp(struct conf::SensorConfig config);
 
 /**
- * Calculate margin offset value.
- *
- * @param[in] setPoint - zone setpoint, integer millidegrees.
- * @param[in] scalar - sensor scalar, floating-point, this is unitless.
- * @param[in] maxTemp - sensor max temp, floating-point degrees.
- * @param[in] targetTemp - sensor target temp, integer millidegrees.
- * @param[in] targetOffset - sensor target temp offset, integer millidegrees.
- * @return Margin offset.
- */
-double calOffsetValue(int setPoint, double scalar, double maxTemp, int targetTemp, int targetOffset = 0);
+  * Calculate margin offset value.
+  *
+  * @param[in] setPointInt - zone setpoint, integer millidegrees.
+  * @param[in] scalar - sensor scalar, floating-point, this is unitless.
+  * @param[in] maxTemp - sensor max temp, floating-point degrees.
+  * @param[in] targetTempInt - sensor target temp, integer millidegrees.
+  * @param[in] targetOffsetInt - sensor target temp offset, integer millidegrees.
+  * @return Margin offset.
+  */
+double calOffsetValue(int setPointInt, double scalar, double maxTemp, int targetTempInt, int targetOffsetInt);
 
 /**
- * Get dbus service name.
- *
- * @param[in] dbusPath - sensor dbus path.
- * @return Dbus service name.
- */
+  * Get dbus service name.
+  *
+  * @param[in] dbusPath - sensor dbus path.
+  * @return Dbus service name.
+  */
 std::string getService(const std::string dbusPath);
 
 /**
- * Update margin temp of zone zoneNum.
- *
- * @param[in] zoneNum - zone number.
- * @param[in] marginTemp - margin temp.
- */
+  * Update margin temp of zone zoneNum.
+  *
+  * @param[in] zoneNum - zone number.
+  * @param[in] marginTemp - margin temp.
+  */
 void updateDbusMarginTemp(int zoneNum, int64_t marginTemp, std::string targetpath);
 
 /**
- * Calculate and update margin temp every second.
- *
- * @param[in] skuConfig - sku config.
- * @param[in] sensorConfig - sensor config.
- */
+  * Calculate and update margin temp every second.
+  *
+  * @param[in] skuConfig - sku config.
+  * @param[in] sensorConfig - sensor config.
+  */
 void updateMarginTempLoop(
-    conf::skuConfig skuConfig,
-    std::map<std::string, struct conf::sensorConfig> sensorConfig);
+    conf::SkuConfig skuConfig,
+    std::map<std::string, struct conf::SensorConfig> sensorConfig);
